e466b73b862656f90b4e6b6cdfd1df27f8b370be
IrynaMasechko94 <165693960+IrynaMasechko94@users.noreply.github.com>
2024-10-08 14:19:11 +0300

    added styles

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import css from './ContactList.module.css';
+
+export default class ContactList extends Component {
+  static defaultProps = {
+    contacts: this.contacts,
+    handleDelete: this.handleDelete,
+  };
+  render() {
+    const { contacts, handleDelete } = this.props;
+    return (
+      <div className={css.wraperContactList}>
+        <ul lassName={css.contactList}>
+          {contacts.map(contact => (
+            <>
+              <li key={contact.id} className={css.contactListItem}>
+                {contact.name}: {contact.number}
+                <button
+                  type="button"
+                  className={css.contactListItemBtn}
+                  onClick={() => handleDelete(contact.id)}
+                >
+                  Delete
+                </button>
+              </li>
+            </>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
